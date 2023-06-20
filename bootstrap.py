c22234d4dfe3be5fb0493ac72e59583c223686d0
Mat <github@matazar.net>
2023-06-19 20:54:47 -0400

    PEP8

--- bootstrap.py
+++ bootstrap.py
@@ -5,13 +5,14 @@ Bootstraps Debian based ansible host.
 ./bootstrap.py <ip/hostname>
 """
 
-from fabric import Connection, Config
 import argparse
 import sys
 import os
 import re
 import socket
 import getpass
+from fabric import Connection, Config
+
 
 class bootstrap(object):
     def __init__(self, args):
@@ -21,7 +22,6 @@ class bootstrap(object):
         self.env = False
         self.ssh_key = self.set_ssh_key()
         self.hosts = self.parse_hosts(args.host)
-        
 
     def __call__(self):
         """
@@ -32,26 +32,35 @@ class bootstrap(object):
             user = input('Username for %s: ' % (host))
             password = getpass.getpass('Password for %s@%s: ' % (user, host))
             if user == 'root':
-                c = Connection(self.hosts[host], user=user, connect_kwargs={"password": password})
+                c = Connection(self.hosts[host],
+                               user=user,
+                               connect_kwargs={"password": password})
                 try:
                     c.run('apt install -y python3 python3-apt')
-                except:
+                except Exception as e:
                     print('Unable to install python with apt.')
+                    print(e)
             else:
                 config = Config(overrides={'sudo': {'password': password}})
-                c = Connection(self.hosts[host], user=user, connect_kwargs={"password": password}, config=config)
+                c = Connection(self.hosts[host],
+                               user=user,
+                               connect_kwargs={"password": password},
+                               config=config)
                 try:
                     c.sudo('apt install -y python3 python3-apt')
-                except:
+                except Exception as e:
                     print('Unable to install python with apt.')
+                    print(e)
             # Ensure the host has a copy of our ssh key.
             c.run('mkdir -p .ssh')
             c.put(self.ssh_key)
-            c.run('cat %s >> .ssh/authorized_keys' % (self.ssh_key.rsplit('/',1)[1]))
+            c.run('cat %s >> .ssh/authorized_keys' %
+                  (self.ssh_key.rsplit('/', 1)[1]))
             c.close()
             # Provide copy/paste command to run bootstrap play.
-            print('\n\nNow run:\nansible-playbook -i %s bootstrap.yml -e "ansible_ssh_user=%s" -D -l %s' % (self.inv_file, user, host))
-    
+            print(f'\n\nNow run:\nansible-playbook -i {self.inv_file} ' +
+                  f'bootstrap.yml -e "ansible_ssh_user={user}" -D -l {host}')
+
     def parse_hosts(self, hosts):
         """
         Ensure hosts is a list of acceptable hosts.
@@ -65,9 +74,25 @@ class bootstrap(object):
         # Variable to keep hosts that pass tests
         results = {}
         # Ensure it's a hostname, ipv4 or ipv6
-        ipv4 = re.compile('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
-        ipv6 = re.compile('(?:^|(?<=\s))(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))(?=\s|$)')
-        hostname = re.compile('^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$', re.IGNORECASE)
+        ipv4 = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
+        ipv6 = re.compile(r'(?:^|(?<=\s))(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]'
+                          r'{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}'
+                          r':){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}'
+                          r'(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}'
+                          r'(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}'
+                          r'(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}'
+                          r'(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}'
+                          r':((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4})'
+                          r'{1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]'
+                          r'{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]'
+                          r'|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|'
+                          r'1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}'
+                          r':((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}'
+                          r'(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))(?=\s|$)')
+        hostname = re.compile(r'^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*'
+                              r'[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z]'
+                              r'[A-Za-z0-9\-]*[A-Za-z0-9])$',
+                              re.IGNORECASE)
         for h in hosts:
             if re.match(ipv4, h):
                 results[h] = h
@@ -91,9 +116,9 @@ class bootstrap(object):
         self.inv_file = os.path.join('env', self.env, 'inventory.yml')
         inv = open(self.inv_file)
         # Loop through the file until we find the host line we want
-        for l in inv.readlines():
-            if l.startswith(host):
-                m = re.search(r'ansible_host=(.+)', l)
+        for line in inv.readlines():
+            if line.startswith(host):
+                m = re.search(r'ansible_host=(.+)', line)
                 if m:
                     return m.group(1)
         print('Unable to find "%s" in %s' % (host, inv.name))
@@ -103,7 +128,7 @@ class bootstrap(object):
         except socket.gaierror:
             print('Unable to resolv "%s"' % (host))
         print('Skipping host %s\n' % (host))
-        return False        
+        return False
 
     def set_env(self):
         """
@@ -130,10 +155,10 @@ class bootstrap(object):
                     self.env = env_opt[int(p)]
                     print('')
                     break
-            except:
-                pass
+            except Exception as e:
+                print(e)
             print('Invalid selection "%s", please choose again.' % (p))
-    
+
     def set_ssh_key(self):
         """
         Prompts the user to select the ssh key they wish to bootstrap with.
@@ -143,7 +168,7 @@ class bootstrap(object):
         ssh_contents = os.listdir(ssh_dir)
         i = 1
         ssh_keys = {}
-        for key in ssh_contents: 
+        for key in ssh_contents:
             if key.endswith('.pub'):
                 print('%d. %s' % (i, key))
                 ssh_keys[i] = os.path.join(ssh_dir, key)
@@ -155,8 +180,8 @@ class bootstrap(object):
                 if int(p) in ssh_keys.keys():
                     print('')
                     return ssh_keys[int(p)]
-            except:
-                pass
+            except Exception as e:
+                print(e)
             print('Invalid selection "%s", please choose again.' % (p))
 
 
@@ -176,5 +201,3 @@ def main():
 
 # if __name__ == '__main__':
 main()
-
-
